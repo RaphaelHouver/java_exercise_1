790195729969bcfd147c466a61be53e47125cd9a
RaphealHouver <raphael13410@gmail.com>
2022-02-10 12:50:06 +0100

    Partie 4 added

--- src/Launcher.java
+++ src/Launcher.java
@@ -6,11 +6,31 @@ public class Launcher {
         Scanner scan = new Scanner(System.in);
         
         String input = scan.nextLine();
-        while (!input.equals("quit"))
+        while (!input.equals("quit") && !input.equals("fibo"))
         {
             System.out.println("Unknown command");
             input = scan.nextLine();
         }
         
+        if (input.equals("fibo"))
+        {
+            System.out.println("Saisissez l'index qui vous souhaitez : ");
+            int n = Integer.parseInt(scan.nextLine());
+            System.out.println(fibo(n));
+        }
+
+        
+    }
+
+    public static int fibo(int n) {
+        if (n == 0) {
+            return n;
+        }
+        else if (n == 1) {
+            return n;
+        }
+        else {
+            return fibo(n-1) + fibo(n-2);
+        }
     }
 }
